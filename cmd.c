284c13df43ccafa682ea1f7f545f8bb2ea1c3716
Dave <davec@lolatravel.com>
2017-02-09 15:03:17 -0500

    Debug/logging added

--- cmd.c
+++ cmd.c
@@ -2,43 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
-char *readline(char *prompt) {
-    char *original;
-    char *smaller;
-    size_t bufsize = 100;
-    int chars;
-
-    original = (char *)malloc(bufsize);
-
-    printf ("%s", prompt);
-	chars = getline(&original, &bufsize, stdin);
-    smaller = (char *)malloc(chars);
-    memset(smaller, chars, 0);
-    strncpy(smaller, original, chars-1);
-    free (original);
-    return smaller;
-}
-
-char *split(char *original, char *delimiter, int lower) {
-    char *split_point = strstr(original, delimiter);
-    int split_pos = split_point == NULL ? strlen(original) : split_point - original;
-    char *word = (char *)malloc(split_pos + 1);
-    int i;
-
-    strncpy (word, original, split_pos);
-
-    if (split_point != NULL) {
-        split_pos++;
-        split_point++;
-        while (*split_point != 0) {
-            *original++ = lower ? tolower(*split_point++) : *split_point++;
-        }
-    }
-    *original = 0;
-
-    return word;
-}
+#include "utils.h"
+#include "asm.h"
 
 int main() {
     char *cmd_line;
@@ -46,15 +11,17 @@ int main() {
     int keepGoing = 1;
 
     while (keepGoing) {
-        cmd_line = readline("EMU> ");
+        cmd_line = readline("EMU> ", stdin);
         command = split(cmd_line, " ", 1);
         if (strcmp(command, "asm") == 0) {
-            printf("Assemble\n");
+            assemble(cmd_line);
         } else if (strcmp(command, "exe") == 0) {
             printf("Execute\n");
         } else if (strcmp(command, "exit") == 0) {
             keepGoing = 0;
         }
+        free (cmd_line);
+        free (command);
     }
 }
 
--- tests/sample.asm
+++ tests/sample.asm
@@ -2,8 +2,8 @@ ADD B,C     ; = 0 0 0 0 0 1 1 0 = 06h
 SUB D,D     ; = 0 0 0 1 1 1 1 1 = 1Fh
 LOAD C,[B]  ; = 0 0 1 0 1 0 0 1 = 29h
 STORE C,[B] ; = 0 0 1 1 1 0 0 1 = 39h
-JLEZ A,B    ; = 0 1 0 0 0 0 0 1 = 41h
-JALR C,D    ; = 0 1 0 1 1 0 1 1 = 5bh
-LUI C, 9    ; = 1 0 1 0 1 0 0 1 = A9h
-LLI B, 2    ; = 1 1 0 1 0 0 1 0 = D2h
+JLEZ  A,B    ; = 0 1 0 0 0 0 0 1 = 41h
+JALR  C,D    ; = 0 1 0 1 1 0 1 1 = 5bh
+LUI   C, 9    ; = 1 0 1 0 1 0 0 1 = A9h
+LLI   B, 2    ; = 1 1 0 1 0 0 1 0 = D2h
 
